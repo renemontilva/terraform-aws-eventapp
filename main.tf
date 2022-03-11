91270aa2697c3ad0d8ec3ce7300a28fc15721c14
Rene Montilva <renemontilva@gmail.com>
2022-03-11 16:13:58 +0100

    Add new version

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "ec2" {
   source             = "app.terraform.io/rene_shiphero/ec2/aws"
-  version            = "1.0.3"
+  version            = "1.0.4"
   instance_type      = var.instance_type
   subnet_id          = var.subnet_id
   security_group_ids = var.security_group_ids
